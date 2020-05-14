63bb22672750a71027a6483f14b670bc2f07f825
Marco <50530429+Marcopy123@users.noreply.github.com>
2020-05-14 13:40:24 -0400

    Update ChangeFileNames.py

--- ChangeFileNames.py
+++ ChangeFileNames.py
@@ -1,53 +1,53 @@
 import os # import os library
 import sys # import sys library
 
-name = '' # initialize name variable
-files_count = 0 # initialize files_count variable
-directories_count = 0 # initialize directories_count variable
+new_name = '' # initialize new_name variable
+file_names_count = 0 # initialize file_names_count variable
+directories_names_count = 0 # initialize directories_names_count variable
 consent = '' # initialize conset variable
 
-for _, directories, files in os.walk(os.getcwd()): # store the directories and files of the                                               # current working directory into directories 
-                                                   # and files variables
-    for file in files:  # check every file
-        if " " in file:  # if file contains space
-            name = file.replace(" ", "_") # assign name to the original file 
-                                          # name with underscore
+for _, directories_names, file_names in os.walk(os.getcwd()): # store the directories_names and file_names of the                                               # current working directory_name into directories_names 
+                                                   # and file_names variables
+    for file_name in file_names:  # check every file_name
+        if " " in file_name:  # if file_name contains space
+            new_name = file_name.replace(" ", "_") # assign new_name to the original file_name 
+                                          # new_name with underscore
 
-            consent = input(f"Would you like to replace {file} to {name} ? ")  # ask for consent to replace
+            consent = input(f"Would you like to replace {file_name} to {new_name} ? ")  # ask for consent to replace
             while consent.lower() != 'no' and consent.lower() != 'yes':
                 consent = input(
-                    f"Would you like to replace {file} to {name} ? ")
+                    f"Would you like to replace {file_name} to {new_name} ? ")
 
             if consent.lower() == "yes":    # if they accept
-                files_count += 1    # increment files_count
-                os.rename(file, name)    # rename the file
-                print(f"Replacing {file} to {name}\n")
+                file_names_count += 1    # increment file_names_count
+                os.rename(file_name, new_name)    # renew_name the file_name
+                print(f"Replacing {file_name} to {new_name}\n")
 
-    for directory in directories:   # check every directory
-        if " " in directory:    # if directory contains space
-            # assign name to the original directory name with underscore
-            name = directory.replace(" ", "_")
+    for directory_name in directories_names:   # check every directory_name
+        if " " in directory_name:    # if directory_name contains space
+            # assign new_name to the original directory_name new_name with underscore
+            new_name = directory_name.replace(" ", "_")
 
             # ask for consent
-            consent = input(f"Would you like to replace {directory} to {name} ? ")
+            consent = input(f"Would you like to replace {directory_name} to {new_name} ? ")
 
             while consent.lower() != 'no' and consent.lower() != 'yes':
                 consent = input(
-                    f"Would you like to replace {directory} to {name} ? ")
+                    f"Would you like to replace {directory_name} to {new_name} ? ")
 
             if consent.lower() == 'yes':    # if they accept
-                directories_count += 1  # increment directories_count
-                os.rename(directory, name)   # rename the directory
-                print(f"Replacing {directory} to {name}\n")
+                directories_names_count += 1  # increment directories_names_count
+                os.rename(directory_name, new_name)   # renew_name the directory_name
+                print(f"Replacing {directory_name} to {new_name}\n")
 
-if files_count + directories_count == 0:    # if nothing has changed
+if file_names_count + directories_names_count == 0:    # if nothing has changed
     print("Did not replace anything")
     sys.exit()  # exit code
 
-if directories_count > 1:   
+if directories_names_count > 1:   
     print(
-        f"Replaced a total of {files_count} file(s) and {directories_count} directories !")
+        f"Replaced a total of {file_names_count} file_name(s) and {directories_names_count} directories_names !")
     sys.exit()
 
 print(
-    f"Replaced a total of {files_count} file(s) and {directories_count} directory !")
+    f"Replaced a total of {file_names_count} file_name(s) and {directories_names_count} directory_name !")
